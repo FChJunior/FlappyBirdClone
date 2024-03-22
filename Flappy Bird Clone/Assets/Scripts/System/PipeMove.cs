131de108cd31ab2235723b573b152e3b548a7929
Chagas <jniors75@gmail.com>
2024-03-21 22:34:08 -0300

    Att Camera

--- Flappy Bird Clone/Assets/Scripts/System/PipeMove.cs
+++ Flappy Bird Clone/Assets/Scripts/System/PipeMove.cs
@@ -10,7 +10,7 @@ public class PipeMove : MonoBehaviour
         if (!GameManager.inPlay) return; // Se o jogo não estiver em andamento, não faz nada
 
         transform.Translate(Vector3.left * speed * Time.deltaTime); // Move o cano para a esquerda
-        if (transform.position.x <= -10f) Destroy(gameObject); // Destroi o cano quando ele sair da tela
+        if (transform.position.x <= -15f) Destroy(gameObject); // Destroi o cano quando ele sair da tela
     }
 
     public void SetSpeed(float s)
